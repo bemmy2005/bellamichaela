45a61abf61cdc6c05a3f10c46257129247904e62
bemmy2005 <emmanuelbugingo@stu.xmu.edu.cn>
2019-08-02 13:28:26 +0800

    Up to signin
    The app chat project buid with nodejs, express, socket.io, mongoose,

---
+++ controllers/users.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports=function(_,passport,User){
+
+  return{
+    SetRouting:function(router){
+      router.get('/',this.indexPage);
+      router.get('/signup',this.getSignUp);
+      router.get('/home',this.homePage);
+
+
+      router.post('/signup', User.signupValidation,this.postSignUp);
+
+    },
+    indexPage:function(req,res){
+      return res.render('index');
+    },
+    getSignUp:function(req,res){
+      const errors=req.flash('error')
+      return res.render('signup',{title:'bellamichaela | Login',message:errors,hasErrors:errors.length>0});
+    },
+    postSignUp: passport.authenticate('local.signup',{
+      successRedirect:'/home',
+      failureRedirect:'/signup',
+      failureFlash:true
+    }),
+   homePage:function(req,res){
+     return res.render('home');
+   }
+
+
+  }
+}
---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const validator = require ('express-validator');
+const ejs =require ('ejs');
+const http =require('http');
+const cookieParser =require ('cookie-parser');
+const session = require('express-session');
+const MongoStore = require ('connect-mongo')(session);
+const mongoose = require ('mongoose');
+const flash = require ('connect-flash');
+const passport = require('passport');
+
+const container = require('./container');
+
+
+
+container.resolve(function(users){
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/bellamuchaela',{useMongoClient: true});
+  const app =  SetupExpress();
+  function SetupExpress(){
+    const app = express();
+    const server = http.createServer(app);
+    //------------this is replaced by below code
+    server.listen(4000, function(){
+     console.log('listening on port 4000');
+  });
+//----------------------------------------------
+  //server.listen(3000, (err) => {
+  //console.log('server started on port: '+3000);
+  // });
+    ConfigureExpress(app);
+    //Setup router
+    const router = require('express-promise-router')();
+    users.SetRouting(router);
+
+    app.use(router);
+  }
+
+
+  function ConfigureExpress(app){
+    require('./passport/passport-local');
+    app.use(express.static('public'));
+    app.use(cookieParser());
+    app.set('view engine','ejs');
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+    //app.use(validator());
+    app.use(session({
+      secret:'thisisasecretkey',
+      resave:false,
+      saveUninitialized:true,
+      store:new MongoStore({mongooseConnection:mongoose.connection})
+    }));
+    app.use(flash());
+    app.use(passport.initialize());
+    app.use(passport.session());
+  }
+
+});
